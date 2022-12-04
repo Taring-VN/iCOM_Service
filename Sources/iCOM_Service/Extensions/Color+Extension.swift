8a185b561ac65cc509fc631829c024918045f1e2
Anh Dan <anhdan.do@gmail.com>
2022-12-04 10:50:01 +0700

    update 221204

---
+++ Sources/iCOM_Service/Extensions/Color+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Extension.swift
+//  CliniTouchVie
+//
+//  Created by Trinh Le on 08/11/2022.
+//
+
+import UIKit
+
+public extension UIColor {
+    convenience init(hex: Int) {
+        let components = (
+            R: CGFloat((hex >> 16) & 0xff) / 255,
+            G: CGFloat((hex >> 08) & 0xff) / 255,
+            B: CGFloat((hex >> 00) & 0xff) / 255
+        )
+        self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
+    }
+}
